006819b2671ed0034292b1d9f26fd07dd61f8819
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-27 15:30:20 -0300

    Update ex004_filtro_registros_tbproduto.sql

--- CONSULTAS/ex004_filtro_registros_tbproduto.sql
+++ CONSULTAS/ex004_filtro_registros_tbproduto.sql
@@ -1,9 +1,24 @@
+/* comando para selecionar o banco de dados sucos. */
 USE `sucos`;
 
+/* antes do update. */
+
+/* mostra todos os produtos cujo sabor é limão. */
+SELECT * FROM tbProduto WHERE SABOR = 'LIMÃO'; # (1)
+
 /* atualiza-se o nome do sabor. caso seja limão, passa a ser, após a atualização, cítricos. */
-UPDATE tbProduto SET SABOR = 'Cítricos' WHERE SABOR = 'Limão';
+UPDATE tbProduto SET SABOR = 'Cítricos' WHERE SABOR = 'Limão'; # (2)
+
+/* depois da atualização(update) do sabor. */
+
+/* mostra todos os produtos cujo sabor é limão, porém, espera-se que não hajam produtos com esse sabor, pois acima foi feito um update que modificava
+o sabor, alterando-o de limão para cítricos. */
+SELECT * FROM tbProduto WHERE SABOR = 'LIMÃO'; # (3)
+
+/* mostra todos os produtos cujo sabor é cítricos. */
+SELECT * FROM tbProduto WHERE SABOR = 'CÍTRICOS'; # (4)
 
 /* como o atributo preco_lista é do tipo ponto flutuante(float), não há uma precisão correta sobre o valor, pois ele fica variando. 
 Deste modo, ao fazer buscas usando o >, <, <>, =, <=, >=, não se obtém o resultado desejado. Para sanar essa falta, usa-se o comando between,
-no qual colocamos os valores em que o valor que queremos pode estar no meio. */
-SELECT * FROM tbProduto WHERE PRECO_LISTA BETWEEN 16.007 AND 16.009;
+no qual colocamos os valores em que o valor que queremos pode estar no meio, ou seja, fazemos uma aproximação. */
+SELECT * FROM tbProduto WHERE PRECO_LISTA BETWEEN 16.007 AND 16.009; # (5)
